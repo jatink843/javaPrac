8e983a6bb9b41361bbb9bf4b03b94fec21ef24ed
Jatin Kumar <jatink843@gmail.com>
2020-08-08 15:31:22 +0530

    adding crypt learning

---
+++ CryptLearning.java
@@ -0,0 +1,32 @@
+import java.security.MessageDigest;
+
+
+public class CryptLearning{
+
+
+	public static void main(String[] args) throws Exception{
+
+
+		String msg = "Pushing Message";
+
+
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(msg.getBytes());
+
+		byte[] out = md.digest();
+		
+		StringBuffer hexstring = new StringBuffer();
+
+		for(int x=0;x<out.length; x++){
+
+			hexstring.append(Integer.toHexString(0xFF & out[x]));
+
+		}
+
+		System.out.println(hexstring.toString());
+
+
+
+	}
+
+}
